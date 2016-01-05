7e07627f67e92b951e8f2a4844c6caab9087cbe3
vincent <vincent78@163.com>
2016-01-05 09:45:31 +0800

    project:添加关于颜色的dependency

---
+++ fertile/Dependency/Wonderful/SXColorLabel.h
@@ -0,0 +1,16 @@
+//
+//  SXColorLabel.h
+//  Wonderful
+//
+//  Created by dongshangxian on 15/12/19.
+//  Copyright © 2015年 Sankuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXColorLabel : UILabel
+
++ (void)setAnotherColor:(UIColor *)color;
++ (void)setAnotherFont:(UIFont *)font;
+
+@end
