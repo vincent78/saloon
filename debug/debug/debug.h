a0b489dc61b05fb93f2fc5c6eaf898f0652e44ca
vincent78 <vincent78@163.com>
2016-06-24 12:51:15 +0800

    工程：添加debug

---
+++ debug/debug/debug.h
@@ -0,0 +1,19 @@
+//
+//  debug.h
+//  debug
+//
+//  Created by vincent on 16/6/24.
+//  Copyright © 2016年 fruit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for debug.
+FOUNDATION_EXPORT double debugVersionNumber;
+
+//! Project version string for debug.
+FOUNDATION_EXPORT const unsigned char debugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <debug/PublicHeader.h>
+
+
